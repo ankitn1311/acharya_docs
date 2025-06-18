5d2a023c5609141492346ba46197b1dd8e5452cc
hnegi2531 <himanshu.dev7350@gmail.com>
2025-06-18 17:59:58 +0530

    changes done

---
+++ src/content/modules/employee/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+    introduction: 'Introduction',
+    'my-menu': 'My Menu',
+    'hr-support': 'HR Support',
+    approver: 'Approver',
+    'salary-reports': 'Salary Reports',
+    'payroll-activities': 'Payroll Activities',
+}; 
