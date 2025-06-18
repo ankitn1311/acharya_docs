5d3283444279f664d614b4d9364157bf60bc88fe
ankitn1311 <ankitn1311@gmail.com>
2025-06-18 15:32:45 +0530

    feat: restructred the folders, added search engine

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,6 @@ const withNextra = nextra({
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
+  output: 'export',
   // ... Add regular Next.js options here
 });
