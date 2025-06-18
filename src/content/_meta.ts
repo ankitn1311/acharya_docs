2b47743f6dab6ad7a6e2a1086f3f328c0ef036e6
ankitn1311 <ankitn1311@gmail.com>
2025-06-18 16:52:32 +0530

    added sections in sidebar, 404 page

---
+++ src/content/_meta.ts
@@ -0,0 +1,12 @@
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  index: 'Acharya ERP Docs',
+  modules: 'Modules',
+  _separator1: { type: 'separator', title: ' ' },
+  _separator2: { type: 'separator', title: 'Contribution' },
+  contribution_guide: 'Contribution Guide',
+  '404': { title: 'Page Not Found', display: 'hidden' }, // This will hide it if supported
+};
+
+export default meta;
