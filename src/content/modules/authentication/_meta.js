03deaaeed9dcc4a569b4588de18c7064f4683bfe
akurath <akurathg@gmail.com>
2025-06-18 15:52:55 +0530

    update authentication module

---
+++ src/content/modules/authentication/_meta.js
@@ -0,0 +1,18 @@
+export default {
+    introduction: 'Introduction',
+    system_architecture: 'System Architecture',
+    technology_stack: 'Technology Stack',
+    database_design: 'Database Design',
+    authenctication_endpoint: 'Authentication Endpoints',
+    user_login_flow: 'User Login Flow',
+    protected_api_access_flow: 'Protected API Access Flow',
+    public_api_access_flow: 'Public API Access Flow',
+    forgot_password_flow: 'Forgot Password Flow',
+    reset_password_flow: 'Reset Password Flow',
+    password_security_policy: 'Password Security Policy',
+    rate_limiting_strategy: 'Rate Limiting Strategy',
+    whatsapp_fallback_communication_strategy: 'WhatsApp Fallback Communication Strategy',
+    error_handling: 'Error Handling & Response Codes',
+    audit_event_categories: 'Audit Event',
+    critical_vulnerabilities_discovered_in_production_system: 'Critical Vulnerabilities in Production Systems'
+}
