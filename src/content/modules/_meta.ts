b985fb17d4a37ad68eb51cd1cb3b2463f0084d28
Ankit Negi <34399945+ankitn1311@users.noreply.github.com>
2025-06-23 22:52:11 +0530

    Update _meta.ts

--- src/content/modules/_meta.ts
+++ src/content/modules/_meta.ts
@@ -2,10 +2,5 @@ export default {
   authentication: 'Authentication',
   institute: 'Institute',
   employee: 'Employee',
-  student: 'Student',
-  academics: 'Academics',
-  hostel: 'Hostel',
-  inventory: 'Inventory',
-  mentorial: 'Mentorial',
-  feedback: 'Feedback',
+  academics: 'Academics'
 }
