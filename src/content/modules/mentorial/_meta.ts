1303575a2ac23758c74865028bc6ffa8eb2137da
Ankit Negi <34399945+ankitn1311@users.noreply.github.com>
2025-06-23 22:43:38 +0530

    Revert "mentorial-module-doc-initial"

--- src/content/modules/mentorial/_meta.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  introduction: 'Introduction',
-  mentor_head: 'Mentor Head',
-  mentor_head_assignment: 'Mentor Head Assignment',
-  mentor_assignment: 'Mentor Assignment',
-  schedule_meeting: 'Schedule Meeting',
-  my_mentee:  'My Mentee',
-}
