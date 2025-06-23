836ad47e185e4ff2d625fe204d17dc94ab8fc053
Ankit Negi <34399945+ankitn1311@users.noreply.github.com>
2025-06-23 22:41:08 +0530

    Revert "student-module-doc-update"

--- src/content/modules/student/admission/_meta.ts
+++ src/content/modules/student/admission/_meta.ts
@@ -1,11 +1,6 @@
-import { read } from "fs";
-
 export default {
   introduction: 'Introduction',
   quick_admission: 'Quick Admission',
   register_walkin_candidate: 'Register Walk-in Candidate',
-  admission_process: 'Admission Process',
-  readmission: 'Readmission',
-  admission_dashboard: 'Admission Dashboard',
-  id_card: 'ID Card',
+  admission_process: 'Admission Process'
 };
--- src/content/modules/student/scholarship/_meta.ts
+++
@@ -1,6 +0,0 @@
-import { read } from "fs";
-
-export default {
-  scholarship_assignment: 'Scholarship Assignment & Verification',
-  scholarship_report: 'Scholarship Report',
-};
