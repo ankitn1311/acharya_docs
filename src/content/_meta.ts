64f9ae31d7f6ebef435634df9ac286f903dd7a36
Ankit Negi <34399945+ankitn1311@users.noreply.github.com>
2025-08-01 21:19:07 +0530

    chore: added github development guidelines (#68)

--- src/content/_meta.ts
+++ src/content/_meta.ts
@@ -6,6 +6,7 @@ const meta: MetaRecord = {
   _separator1: { type: 'separator', title: ' ' },
   _separator2: { type: 'separator', title: 'Contribution' },
   contribution_guide: 'Contribution Guide',
+  development_flow: 'Development Flow',
   '404': { title: 'Page Not Found', display: 'hidden' }, // This will hide it if supported
 };
 
